b1accf16ab1de6a08291ae49e7de3f707f1e9ee6
nisa <knitandtoss.indonesia@gmail.com>
2024-08-02 01:09:10 +0000

    bind enter to mulai baru

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useCallback, useEffect, useContext } from "react";
+
 import { AppContext } from "../App";
 
 function GameOver() {
@@ -11,8 +12,26 @@ function GameOver() {
     correctWord,
     onDelete,
   } = useContext(AppContext);
+  const handleKeyboard = useCallback(
+    (event) => {
+      if (gameOver.gameOver) {
+        if (event.key === "Enter") {
+          window.location.replace("https://katlaxoo.vercel.app");
+        }
+      }
+      else return;
+    }
+  );
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
   return (
-    <div className="gameOver">
+    <div className="gameOver" onKeyDown={handleKeyboard}>
       <h3>{gameOver.guessedWord ? "Kamu benar!" : "Nice try bos"}</h3>
       <h3>Jawabannya {correctWord.toUpperCase()}</h3>
       {gameOver.guessedWord}
